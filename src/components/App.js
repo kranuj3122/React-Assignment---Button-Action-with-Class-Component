5fe3c53b58b7e59e0df6a21b3c8ae691822b5b09
kranuj3122 <43272788+kranuj3122@users.noreply.github.com>
2021-02-10 03:31:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import Para from './Para';
-import './App.css';
-
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state={
-      flag:false
+  constructor(props) {
+    super(props);
+    this.state = {
+      flag: false
     };
   }
-   handleClick= ()=> {
-    this.setState({flag:true});
+  handleClick = () => {
+    this.setState({ flag: true });
   }
   render() {
     return (
       <div id='main'>
-      {/* Do not remove this main div!! */}
-      <button id="click" onClick={this.handleClick}>click me</button>
-      {this.state.flag?<Para/>:''}
+      { /* Do not remove this main div!! */}
+        <button id="click" onClick={this.handleClick}>click me</button>
+        {this.state.flag && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
       </div>
     );
   }
