4f8a5c75e07ba95c93366b2c03b9e2fb8409efe8
kranuj3122 <43272788+kranuj3122@users.noreply.github.com>
2021-02-09 22:36:57 +0530

    Update Para.js

--- src/components/Para.js
+++ src/components/Para.js
@@ -2,7 +2,7 @@ import React from 'react';
 class Para extends React.Component {
     render() {
         return (
-            <p id="para" >Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+            <p id="para" >Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>
         );
     }
 }
