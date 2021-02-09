85a632a384f5634c34971028d73691e4f43f1819
kranuj3122 <43272788+kranuj3122@users.noreply.github.com>
2021-02-10 03:29:04 +0530

    Delete Para.js

--- src/components/Para.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-class Para extends React.Component {
-    render() {
-        return (
-            <p id="para" >Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-        );
-    }
-}
-export default Para;
