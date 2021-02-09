28478abc7c0f8e3ea42106d751e56eae0217e1f6
kranuj3122 <43272788+kranuj3122@users.noreply.github.com>
2021-02-10 03:25:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,7 @@ class App extends React.Component {
   render() {
     return (
       <div id='main'>
+      {/* Do not remove this main div!! */}
       <button id="click" onClick={this.handleClick}>click me</button>
       {this.state.flag?<Para/>:''}
       </div>
