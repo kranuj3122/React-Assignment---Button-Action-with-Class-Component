fb7d0c9baf2e00a30c49df74391f4e89bd514da8
kranuj3122 <43272788+kranuj3122@users.noreply.github.com>
2021-02-10 14:03:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
       <div id='main'>
       { /* Do not remove this main div!! */}
         <button id="click" onClick={this.handleClick}>click me</button>
-        {this.state.flag && <p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+        {this.state.flag && <p id='para'>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
       </div>
     );
   }
